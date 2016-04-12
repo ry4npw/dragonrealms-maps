6ebe8134bfe510b47454c078fac7c21cb23a40b3
Ryan Powell <mrpowell@gmail.com>
2016-04-12 16:54:31 -0400

    rendering the mainland

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,9 @@ var $ = require('jQuery');
 var Konva = require('konva');
 var x2js = new X2JS();
 
-var mapScaleFactor, roomSize, showLabels, squareLayer, lineLayer, labelLayer, roomsGroup, connectors;
+var mapScaleFactor, roomSize, showLabels, squareLayer, lineLayer, labelLayer, roomsGroup, connectors, xmin, xmax, ymin, ymax;
 
-mapScaleFactor = 1;
+mapScaleFactor = 0.5;
 roomSize = 8 * mapScaleFactor;
 showLabels = false;
 squareLayer = new Konva.Layer();
@@ -15,6 +15,10 @@ lineLayer = new Konva.Layer();
 labelLayer = new Konva.Layer();
 roomsGroup = new Konva.Group();
 connectors = [];
+xmin = 0;
+xmax = 0;
+ymin = 0;
+ymax = 0;
 
 function drawLines() {
 	for (var key in connectors) {
@@ -104,6 +108,20 @@ function addRoom(id, x, y, color) {
 
 	// add the shape to the layer
 	roomsGroup.add(room);
+
+	// save room details
+	if (x > xmax) {
+		xmax = x;
+	}
+	if (x < xmin) {
+		xmin = x;
+	}
+	if (y > ymax) {
+		ymax = y;
+	}
+	if (y < ymin) {
+		ymin = y;
+	}
 }
 
 function addBlankRoom(id, x, y) {
@@ -277,7 +295,7 @@ function getMap(name) {
 	}).responseText);
 }
 
-function stitchMaps(map, mapList, renderedMaps) {
+function stitchMaps(map, mapList, mapsToRender, renderedMaps) {
 	if (!map || !map.zone || !map.zone.node) {
 		console.log("map is not valid!");
 		return;
@@ -289,11 +307,11 @@ function stitchMaps(map, mapList, renderedMaps) {
 
 		if (room._note) {
 			var mapName = room._note.split('|')[0];
-			if (mapName.endsWith('xml') && renderedMaps.indexOf(mapName) === -1) {
+			if (mapName.endsWith('xml') && mapsToRender.indexOf(mapName) > -1 && renderedMaps.indexOf(mapName) === -1) {
 				// found a linked map, align the matching rooms
 				mapList[mapName] = offsetMap(room, getMap(mapName));
 				renderedMaps.push(mapName);
-				stitchMaps(mapList[mapName], mapList, renderedMaps);
+				stitchMaps(mapList[mapName], mapList, mapsToRender, renderedMaps);
 			}
 		}
 	}
@@ -327,18 +345,93 @@ function offsetMap(room1, map) {
 }
 
 $(function() {
-	var stage = new Konva.Stage({
-		container: 'container',
-		width: window.innerWidth,
-		height: window.innerHeight,
-		draggable: true
-	});
-
 	// load map data
 	console.log("stitching maps");
 	var mapList = [];
 	mapList["Map1_Crossing.xml"] = normalizeMapPoints(getMap("Map1_Crossing.xml"));
-	stitchMaps(mapList["Map1_Crossing.xml"], mapList, ["Map1_Crossing.xml", "Map42_Map42_Therenborough_and_Keep.xml", "Map150_The_Ways.xml", "HollowEve.xml", "Guildfest.xml"]);
+	var mapsToRender = [
+		//"Map1_Crossing.xml",
+		"Map8_Crossing_East_Gate.xml",
+		"Map7_NTR.xml",
+		"Map13_Dirge.xml",
+		"Map14c_Faldesu.xml",
+		"Map31_Riverhaven_East_Gate.xml",
+		"Map30_Riverhaven.xml",
+		"Map33_Riverhaven_West_Gate.xml",
+		"Map33a_Road_to_Therenborough.xml",
+		"Map34_Mistwood_Forest.xml",
+		"Map34a_Rossman's_Landing.xml",
+		"Map40_Langenfirth_to_Therenborough.xml",
+		"Map42_Therenborough_and_Keep.xml",
+		"Map35_Throne_City.xml",
+		//"Map998_Transports.xml",
+		//"Map98_Road_To_Aesry.xml",
+		//"Map98a_Sea_Caves.xml",
+		//"Map99_Aesry_Surlaenis'a.xml",
+		//"Map150_Fang_Cove.xml",
+		//"Map90_Ratha.xml",
+		//"Map90a_Sand_Sprites_Ochre_La'heke.xml",
+		//"Map90d_Taisgath.xml",
+		//"Map95_Ratha_NE.xml",
+		//"Map92_Ratha_NW.xml",
+		"Map47_Muspar'i.xml",
+		//"Map47a_Muspar'i_Thief.xml",
+		"Map48_Haizen_Cugis.xml",
+		"Map41_Marauders.xml",
+		"Map61_Leth_Deriel.xml",
+		"Map60_STR1.xml",
+		//"Map1a_Crossing_Thief.xml",
+		"Map50_Segoltha_River.xml",
+		"Map4a_Tiger_Clan.xml",
+		"Map4_Crossing_West_Gate.xml",
+		"Map2_Lake_of_Dreams.xml",
+		"Map6_Crossing_North_Gate.xml",
+		"Map112_Ilaya_Taipa.xml",
+		"Map113_West_Segoltha.xml",
+		"Map123_Himineldar_Shel.xml",
+		"Map116_Hibarnhvidar.xml",
+		"Map118e_Firecats.xml",
+		"Map126_Hawstkaal_Road.xml",
+		"Map127_Boar_Clan.xml",
+		"Map69_Shard_West_Gate.xml",
+		"Map66_STR3.xml",
+		"Map68_Shard_South_Gate.xml",
+		"Map67_Shard.xml",
+		//"Map67a_Shard_Thief.xml",
+		//"Map68_shard_south_gate.xml",
+		"Map68b_Lost_Ground.xml",
+		"Map68a_Ice_Caves.xml",
+		"Map65_Under_the_Gondola.xml",
+		"Map62_STR2.xml",
+		"Map63_Oshu'ehhrsk_Manor.xml",
+		"Map114_Ain_Ghazal.xml",
+		"Map58_Acenamacra_Village.xml",
+		"Map59_Boggy_Wood.xml",
+		//"Map107_Mer'Kresh.xml",
+		//"Map107a_Belarritaco_Bay.xml",
+		//"Map108_M'Riss.xml",
+		"Map40a_Langenfirth_to_RakashTown.xml",
+		"Map32_Riverhaven_North_Gate.xml",
+		//"Map30b_Riverhaven_Thief.xml",
+		//"Map30a_Dunshade_Manor.xml",
+		"Map31a_Zaulfung.xml",
+		"Map31b_Maelshyve's_Fortress.xml",
+		"Map14b_Greater_Fist.xml",
+		//"Map7a_Vineyard.xml",
+		"Map11_Vipers_Rock_Guardians.xml",
+		"Map10_Abandoned_Mine_Lairocott.xml",
+		"Map9b_Sorrow's_Reach.xml",
+		"Map12a_Misenseor_Abbey.xml",
+		//"Drathroks_Duskruin.xml",
+		//"Map8a_Lost_Crossing.xml"
+		//"Map1m_Crossing_Jadewater.xml",
+		//"Map2a_Temple.xml",
+		//"Map2d_Escape_Tunnels.xml",
+		//"Map1l_Crossing_Amusement_Pier.xml",
+		//"Map1j_Market_Plaza.xml"
+	];
+	var renderedMaps = ["Map1_Crossing.xml"];
+	stitchMaps(mapList["Map1_Crossing.xml"], mapList, mapsToRender, renderedMaps);
 
 	if (!mapList) {
 		console.log("failed to fetch map list");
@@ -354,15 +447,30 @@ $(function() {
 	// add rooms to layer
 	squareLayer.add(roomsGroup);
 
+	var stage = new Konva.Stage({
+		container: 'container',
+		width: Math.abs(parseInt(xmin)) + Math.abs(parseInt(xmax)) + roomSize,
+		height: Math.abs(parseInt(ymin)) + Math.abs(parseInt(ymax)) + roomSize,
+		draggable: true
+	});
+
+	squareLayer.setOffsetX(xmin);
+	squareLayer.setOffsetY(ymin);
+
 	// labels on top of squares on top of lines
-	//stage.add(lineLayer);
+	stage.add(lineLayer);
+	drawLines();
 	stage.add(squareLayer);
-	//drawLines();
 
 	if (showLabels) {
 		stage.add(labelLayer);
 		drawLabels();
 	}
 
+	// Uncomment below to pop up the result as a PNG in a new window/tab
+	//var dataUrl = stage.toDataURL({
+	//	mimeType: "image/png"
+	//});
+	//window.open(dataUrl);
 });
 
